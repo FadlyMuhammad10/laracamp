eda29391912ccc375b63c5516a332153fa7b8791
FadlyMuhammad10 <davit9747@gmail.com>
2022-07-07 20:02:51 +0700

    finish migration camp,camp benefit,checkout

---
+++ database/migrations/2022_07_07_123424_create_camp_benefits_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+
+            //cara 1
+            //$table->bigInteger('camp_id')->unsigned();
+            //$table->unsignedBigInteger('camp_id');
+
+            //cara 2
+            $table->foreignId('camp_id')->constrained();
+
+            $table->string('name');
+            $table->timestamps();
+
+            //cara 1
+            //$table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
